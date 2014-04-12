d4913d965956bc78b3640417be92feaeca7d5c92
aero-z <aero_super@yahoo.com>
2014-04-11 21:57:48 -0400

    first version of heuristic

--- src/main/scala/heuristic2048/Heuristic.scala
+++ src/main/scala/heuristic2048/Heuristic.scala
@@ -5,7 +5,12 @@ object Heuristic {
     Move.all.maxBy(m => score(state.move(m)))
   }
   
-  private def score(state: GameState, depth: Int = 0): Int = {
-    2
+  private def score(state: GameState, depth: Int = 4): Float = {
+    def emptyCount(state: GameState) = state.cells.flatten.count(_ == EmptyCell)
+
+    depth match {
+      case 0 => emptyCount(state)
+      case _ => Move.all.map(move => score(state.move(move), depth - 1)).sum.toFloat / Move.all.length
+    }
   }
 }
